20e51175d33f045df9449aa396d88b903cf4b6b6
ShadanKafil <shadankafil223@gmail.com>
2021-01-20 12:39:03 +0530

    aboutUs updated

--- javascript.js/index.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
