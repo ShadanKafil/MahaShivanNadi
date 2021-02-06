80de1698133b6b3e7a261167e12894d853ee3d80
ShadanKafil <shadankafil223@gmail.com>
2021-02-06 16:52:36 +0530

    nadiAstrology update

---
+++ javascript/nadiAstrology.js
@@ -0,0 +1,40 @@
+function myFunctiontamil(){
+    document.getElementById("nav-tabContent-tamil").style.display = "block";
+    document.getElementById("nav-tabContent-english").style.display = "none";
+    document.getElementById("nav-tabContent-hindi").style.display = "none";
+    document.getElementById("nav-tabContent-telugu").style.display = "none";
+    document.getElementById("nav-tabContent-kanada").style.display = "none";
+}
+
+
+function myFunctiontelugu(){
+    document.getElementById("nav-tabContent-telugu").style.display = "block";
+    document.getElementById("nav-tabContent-english").style.display = "none";
+    document.getElementById("nav-tabContent-tamil").style.display = "none";
+    document.getElementById("nav-tabContent-hindi").style.display = "none";
+    document.getElementById("nav-tabContent-kanada").style.display = "none";
+}
+
+function myFunctionhindi(){
+    document.getElementById("nav-tabContent-hindi").style.display = "block";
+    document.getElementById("nav-tabContent-english").style.display = "none";
+    document.getElementById("nav-tabContent-tamil").style.display = "none";
+    document.getElementById("nav-tabContent-telugu").style.display = "none";
+    document.getElementById("nav-tabContent-kanada").style.display = "none";
+}
+
+function myFunctionkanada(){
+    document.getElementById("nav-tabContent-kanada").style.display = "block";
+    document.getElementById("nav-tabContent-english").style.display = "none";
+    document.getElementById("nav-tabContent-tamil").style.display = "none";
+    document.getElementById("nav-tabContent-telugu").style.display = "none";
+    document.getElementById("nav-tabContent-hindi").style.display = "none";
+}
+
+function myFunctionenglish(){
+    document.getElementById("nav-tabContent-english").style.display = "block";
+    document.getElementById("nav-tabContent-kanada").style.display = "none";
+    document.getElementById("nav-tabContent-tamil").style.display = "none";
+    document.getElementById("nav-tabContent-telugu").style.display = "none";
+    document.getElementById("nav-tabContent-hindi").style.display = "none";
+}
