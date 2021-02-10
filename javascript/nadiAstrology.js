9f170786505b9822a26f77bfcd845d2214e97df3
ShadanKafil <shadankafil223@gmail.com>
2021-02-10 15:34:18 +0530

    added back to top button

--- javascript/nadiAstrology.js
+++ javascript/nadiAstrology.js
@@ -95,3 +95,15 @@ function selectedLanguage(selectedLanguage) {
     }
 
 }
+
+
+// start of back to top button
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if(window.pageYOffset >100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
